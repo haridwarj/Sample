99b3dc77df1da8ec5b045557188493dc0fa5f5ea
haridwarj <haridwarj@suntecgroup.com>
2014-08-18 12:20:51 +0530

    7464 {master} [PMS 00007]
    Inner Class Concept.

---
+++ Check/src/org/TestOuterClass.java
@@ -0,0 +1,15 @@
+package org;
+
+public class TestOuterClass {
+	private int a=8;
+	private static class TestInnerClass{
+		
+	}
+	
+	class InnerClass{
+		int b=9;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
